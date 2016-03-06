c4075e1a0895348bf3b2eb26c30aed375c5c6328
S.H <h3shiri@gmail.com>
2016-03-06 20:41:36 +0200

    Basic single move class

--- ex1/src/Move.java
+++ ex1/src/Move.java
@@ -1,5 +1,55 @@
 /**
- * Created by shiri on 3/4/16.
+ * The Move class represents a move in the Nim game by a player.
+ * A move consists of the row on which it is applied, the left bound (inclusive) of the sequence of
+ * sticks to mark, and the right bound (inclusive) of the same sequence.
  */
 public class Move {
+	/** data members of the move object */
+	private int inRow;
+	private int inLeft;
+	private int inRight;
+
+	/**
+	 * Constructs a Move object with the given parameters
+	 * @param inRow - The row on which the move is performed.
+	 * @param inLeft - The left bound of the sequence to mark.
+	 * @param inRight - The right bound of the sequence to mark.
+	 */
+	Move(int inRow, int inLeft, int inRight){
+		this.inRow = inRow;
+		this.inLeft = inLeft;
+		this.inRight = inRight;
+	}
+
+	/**
+	 * @return The left bound of the stick sequence to mark.
+	 */
+	public int getLeftBound(){
+		return inLeft;
+	}
+
+	/**
+	 * @return The right bound of the stick sequence to mark.
+	 */
+	public int getRightBound(){
+		return inRight;
+	}
+
+	/**
+	 * @return The row on which the move is performed.
+	 */
+	public int getRow(){
+		return inRow;
+	}
+
+	/**
+	 * @return a string representation of the move. For example, if the row is 2,
+	 * the left bound of the sequence is 3 and the right bound is 5, this function will return
+	 * the string "2:3-5" (without any spaces).
+	 */
+	public String toString(){
+		String res =  ((getRow())+":"+(getLeftBound())+"-"+(getRightBound()));
+		return res;
+	}
+
 }
