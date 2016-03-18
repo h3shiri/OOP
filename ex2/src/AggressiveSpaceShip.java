3e85de1a81a16aaf8dde43a834a582ebb707ac2f
S.H <h3shiri@gmail.com>
2016-03-19 01:02:17 +0200

    pump it up

---
+++ ex2/src/AggressiveSpaceShip.java
@@ -0,0 +1,27 @@
+import oop.ex2.*;
+import java.awt.Image;
+import java.awt.*;
+
+public class AggressiveSpaceShip extends SpaceShip{
+
+	/**
+     * This is called once per round by the SpaceWars game driver.
+     * defines the ship behaviour, preforms according to the Aggressive behaviour.
+     * @parm game the game object to which this ship belongs.
+     */
+    public void doAction(SpaceWars game) {
+        
+    }
+
+    /** 
+     * returns the ships image.
+     * @return Image of the computer ship
+     */
+    public Image getImage() {
+        if (shieldsCondition) {
+        	return GameGui.ENEMY_SPACESHIP_IMAGE_SHIELD;
+        }
+        else
+        	return GameGui.ENEMY_SPACESHIP_IMAGE;
+    }
+}
