c267000b172cd9d8d22f78dcc136161481ff38a6
S.H <h3shiri@gmail.com>
2016-06-09 13:23:13 +0300

    Ace

--- ex5/src/filters/NegativeDoubleException.java
+++
@@ -1,9 +0,0 @@
-package filters;
-
-public class NegativeDoubleException extends FilterNumberException{
-	private static final long serialVersionUID = 1L;
-
-	public NegativeDoubleException(){
-		super("A negative value has been entered inappropriately");
-	}
-}
